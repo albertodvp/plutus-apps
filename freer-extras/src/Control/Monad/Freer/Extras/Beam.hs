09c191a041a12b473fdf484ff6725967c99e8f1f
Alberto Fanton <alberto.fanton@protonmail.com>
2023-12-12 01:55:37 +0100

    Fix haddock comment

--- freer-extras/src/Control/Monad/Freer/Extras/Beam.hs
+++ freer-extras/src/Control/Monad/Freer/Extras/Beam.hs
@@ -157,10 +157,11 @@ data BeamEffect r where
     => SqlSelect Sqlite a
     -> BeamEffect (Maybe a)
 
-  SqlCommand :: Sqlite.Query -> BeamEffect ()
-  -- ^ mainly to execute sql commands in between sql queries
-  -- e.g., DROP INDEX AND CREATE INDEX before insertions, delete or updates
-  -- This is necessary on large DB
+  SqlCommand :: Sqlite.Query
+    -> BeamEffect ()
+    -- ^ mainly to execute sql commands in between sql queries
+    -- e.g., DROP INDEX AND CREATE INDEX before insertions, delete or updates
+    -- This is necessary on large DB
 
   Combined
     :: [BeamEffect ()]
